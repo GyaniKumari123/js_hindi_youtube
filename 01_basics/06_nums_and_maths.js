7ed8865170ce2b39b0d6e46cc83141eda89b6c58
Gyani_Kumari_08 <gyanik.ug24.cs@nitp.ac.in>
2026-05-24 11:19:24 +0000

    nums and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+// const score=400;
+// console.log(400);
+// const value =new Number(500);
+// console.log(value);
+// // many Number methods available;
+// console.log(score.toPrecision(6))
+// ++++++++++MATH+++++++
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.6))
+// console.log(Math.ceil(5.4))  
+// console.log(Math.floor(2.5));
+// console.log(Math.max(1,2,3,4,5,8))
+// console.log(Math.min(1,2,3,4,5,6))
+console.log(Math.random())
+// Math.random value always lies between zeros and ones
+// To shift one digit multiply Math.random()*10
+console.log(Math.random()*10)
+ // to avoid 0.03 type numbers Math.random()*10+1
+ console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.random() * (max-min+1))
+console.log(Math.floor(Math.random() * (max-min+1)+min))
+
+
---
+++ 01_basics/07_dates_injs.js
@@ -0,0 +1,35 @@
+// Dates
+let myDate=new Date();
+// console.log(typeOf myDate);
+// Date is an object in javascript
+console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.toUTCStrin(());
+
+// let myCreateDate=new Date(2025,0,29);
+let myCreateDate=new Date("2025-01-29");
+// let createNewDate=new Date("10-10-2006");
+// console.log(myCreateDate.toString());
+// console.log(myCreateDate.toLocaleDateString());
+// console.log(createNewDate.toLocaleDateString());
+
+// let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime()); // we got some milisecond
+// console.log(myCreateDate.getTime()/1000) 
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+// to convert that miliseconds 
+// into seconds for easily 
+// compare the value with others divide by 1000
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getDate())
+
